49a94afc6804e077d40414d1a4873b520a507d4b
read-cycle <nikhil29877@gmail.com>
2025-11-11 20:39:54 +0530

    Final

--- src/components_landing/ContactUs.vue
+++ src/components_landing/ContactUs.vue
@@ -31,7 +31,7 @@
         <textarea name="message" rows="4" required placeholder="What's on your mind?"></textarea>
       </label>
     </div>
-    <p class="mail-para"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg> readcycle-inv@gmail.com</p>
+    <p class="mail-para"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg> read.cycle.inv@gmail.com</p>
     <div class="form-button-row">
       <button type="submit">Send Message</button>
     </div>
